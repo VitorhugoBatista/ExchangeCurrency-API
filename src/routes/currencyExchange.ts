da3aece4c217e3f33d3d0949bc39a5143a47e511
vitorhugo <vitorhugosjrp@gmail.com>
2024-02-24 06:23:28 -0300

    feature: Add Swagger JSON for API documentation
    Implemented a comprehensive Swagger JSON file to encapsulate the API's documentation. This step centralizes the specification, facilitating easier updates and maintenance. The integration with swagger-jsdoc and swagger-ui-express has been adjusted to load the API documentation directly from this JSON file, enhancing the documentation's accessibility and readability for development and API consumption.

--- src/routes/currencyExchange.ts
+++ src/routes/currencyExchange.ts
@@ -3,93 +3,9 @@ import { convertCurrencyController } from '../controllers/currencyController';
 import { listConvertCurrencyController } from '../controllers/listCurrencyController';
 
 const router = Router();
-/**
- * @swagger
- * /v1/currencyexchange/list/{userId}:
- *   get:
- *     summary: Lists currency conversions by user
- *     tags: [Conversions]
- *     parameters:
- *       - in: path
- *         name: userId
- *         schema:
- *           type: integer
- *         required: true
- *         description: User ID
- *     responses:
- *       200:
- *         description: List of currency conversions for the user
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 type: object
- *                 properties:
- *                   sourceCurrency:
- *                     type: string
- *                   targetCurrency:
- *                     type: string
- *                   sourceValue:
- *                     type: number
- *                   targetValue:
- *                     type: number
- *                   conversionRate:
- *                     type: number
- *                   date:
- *                     type: string
- *                     format: date-time
- */
 
 router.get('/list/:userId', listConvertCurrencyController);
 
-/**
- * @swagger
- * /v1/currencyexchange/:
- *   post:
- *     summary: Converts values from one currency to another
- *     tags: [Conversions]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               userId:
- *                 type: integer
- *               fromCurrency:
- *                 type: string
- *               toCurrency:
- *                 type: string
- *               amount:
- *                 type: number
- *     responses:
- *       200:
- *         description: Conversion result
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 userId:
- *                   type: integer
- *                 sourceCurrency:
- *                   type: string
- *                 targetCurrency:
- *                   type: string
- *                 sourceValue:
- *                   type: number
- *                 targetValue:
- *                   type: number
- *                 date:
- *                   type: string
- *                   format: date-time
- *                 conversionRate:
- *                   type: number
- *                 id:
- *                   type: integer
- */
 router.post('/', convertCurrencyController);
 
 export default router;
--- swaggerOptions.ts
+++ swaggerOptions.ts
@@ -1,13 +1,11 @@
 import { Options } from 'swagger-jsdoc';
+import path from 'path';
+import fs from 'fs';
+
+const swaggerDocumentPath = path.join(__dirname, 'src/docs/swaggerDocs.json');
+const swaggerDocument = JSON.parse(fs.readFileSync(swaggerDocumentPath, 'utf-8'));
 
 export const swaggerOptions: Options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'API Exchange Currency',
-      version: '1.0.0',
-      description: 'Documentation for Exchange Currency API',
-    },
-  },
+  definition: swaggerDocument,
   apis: ['./dist/src/routes/*.js', '../src/routes/*.ts'], 
 };
